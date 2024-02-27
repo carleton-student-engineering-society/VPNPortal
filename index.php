29f9b3d74b549a26b0851dc070955494c0935714
finlaywashere <finman292004@protonmail.com>
2024-02-27 09:33:41 -0500

    Added index page

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<form method="POST" action="vpn.php">
+<label for="username">Username: </label><input id="username" name="username" type="text"><br>
+<label for="password">Password: </label><input id="password" name="password" type="password"><br>
+<label for="group">Network: </label><select name="group" id="group">
+<option value="csesnet">CSESNet</option>
+<option value="afnet">AFNet</option>
+</select>
+<input id="submit" name="submit" type="submit">
+</form>
+
+<p>
+Contact the CSES Technical Officer with any questions or problems at <a href="mailto:technical@cses.carleton.ca">technical@cses.carleton.ca</a>
+</p>
+
+</html>
