0ddfcbccad381178cef2365a51eba18149246dac
Finlay Maroney <finlay@finlaym.xyz>
2024-05-02 17:27:16 -0400

    Added gen cert C wrapper

---
+++ gen_cert.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+    if (argc != 3){
+        printf("Usage: ./gen_cert <dir> <username>");
+        return 1;
+    }
+    if (strcmp("easy-rsa", argv[1]) != 0 && strcmp("affiliate-rsa", argv[1]) != 0){
+        printf("Invalid directory!");
+        return 2;
+    }
+    if (strlen(argv[2]) > 100){
+        printf("Invalid username!");
+        return 3;
+    }
+    for(int i = 0; i < strlen(argv[2]); i++){
+        if (!isalnum(argv[2][i])){
+            printf("Invalid username!");
+            return 3;
+        }
+    }
+    setuid(0);
+    system("/var/www/html/VPNPortal/gen_cert.sh");
+    return 0;
+}
